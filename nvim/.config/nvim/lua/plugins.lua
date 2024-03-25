29e6b4ad8db06a0291feabb4b5655aa3df97b52f
John Whiles <johnwhiles@outlook.com>
2024-03-25 09:52:59 +0000

    don't use nightly for nvim tree

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -34,8 +34,7 @@ return require('packer').startup(function(use)
       'nvim-tree/nvim-tree.lua',
       requires = {
         'nvim-tree/nvim-web-devicons', -- optional, for file icons
-      },
-      tag = 'nightly' -- optional, updated every week. (see issue #1193)
+      }
     }
 
     -- the testing zone
