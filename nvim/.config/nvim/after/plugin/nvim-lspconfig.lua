d763324a81aa451473f6536f2412b61418a94d83
John Whiles <john.whiles@consensys.net>
2024-10-24 17:30:02 +0100

    tsserver -> ts_ls

--- nvim/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ nvim/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require('lspconfig')['tsserver'].setup{
+require('lspconfig')['ts_ls'].setup{
   on_attach = on_attach,
   flags = lsp_flags,
 }
