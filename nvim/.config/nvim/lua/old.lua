bdbb5e3b2a6c6c012b8437d22b7b86ce9c92d93c
John Whiles <john.whiles@consensys.net>
2025-07-21 14:18:14 +0100

    remove neoterm

--- nvim/.config/nvim/lua/old.lua
+++ nvim/.config/nvim/lua/old.lua
@@ -4,20 +4,6 @@ if has('nvim')
   au! FileType fzf tunmap <buffer> <Esc>
 endif
 
-" kassio/neoterm
-let g:neoterm_default_mod = 'vertical'
-" let g:neoterm_size = 100
-let g:neoterm_autoinsert = 1
-let g:neoterm_autoscroll = 1
-let g:neoterm_term_per_tab = 1
-nnoremap <c-y> :Ttoggle<CR>
-inoremap <c-y> <Esc>:Ttoggle<CR>
-tnoremap <c-y> <c-\><c-n>:Ttoggle<CR>
-nnoremap <leader>x :TREPLSendLine<CR>
-vnoremap <leader>x :TREPLSendSelection<CR>
-if has('nvim')
-  au! TermOpen * tnoremap <buffer> <Esc> <c-\><c-n>
-endif
 
 
 "" automatically update on file changes
