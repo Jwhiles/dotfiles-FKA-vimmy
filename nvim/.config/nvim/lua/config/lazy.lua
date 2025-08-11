82c4ecdf3c1e5ac4b70b065904dc97f1e76b4e39
John Whiles <john.whiles@consensys.net>
2025-08-11 13:47:09 +0100

    don't notify on every nvim startup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -31,5 +31,5 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
-  checker = { enabled = true },
+	checker = { enabled = true, notify = false }
 })
