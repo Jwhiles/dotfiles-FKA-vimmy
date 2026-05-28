2e789bb17d2f74acb73876418e48a1608af2ce9f
John Whiles <john.whiles@consensys.net>
2026-05-28 10:39:37 +0100

    add slint to lsp config

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,62 +2,65 @@
 -- https://github.com/neovim/nvim-lspconfig
 
 local common_on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
 	local function map(key, func, desc)
-		vim.keymap.set('n', key, func, vim.tbl_extend('force', bufopts, {desc = desc}))
+		vim.keymap.set("n", key, func, vim.tbl_extend("force", bufopts, { desc = desc }))
 	end
 
-	map('gD', vim.lsp.buf.declaration, 'Go to declaration')
-	map('K', vim.lsp.buf.hover, 'Show hover information')
-	map('gi', vim.lsp.buf.implementation, 'Go to implementation')
-	map('<C-k>', vim.lsp.buf.signature_help, 'Show signature help')
-	map('<space>D', vim.lsp.buf.type_definition, 'Go to type definition')
-	map('<space>rn', vim.lsp.buf.rename, 'Rename symbol')
-	map('<space>ca', vim.lsp.buf.code_action, 'Code actions')
-  map('<leader>q', vim.diagnostic.setqflist, "Populate quick fix list with diagnostics")
-  map('<leader>f', function() 
-  	vim.lsp.buf.format { filter = function(client) return client.name ~= "ts_ls" end } 
-  end, "Format buffer") 
+	map("gD", vim.lsp.buf.declaration, "Go to declaration")
+	map("K", vim.lsp.buf.hover, "Show hover information")
+	map("gi", vim.lsp.buf.implementation, "Go to implementation")
+	map("<C-k>", vim.lsp.buf.signature_help, "Show signature help")
+	map("<space>D", vim.lsp.buf.type_definition, "Go to type definition")
+	map("<space>rn", vim.lsp.buf.rename, "Rename symbol")
+	map("<space>ca", vim.lsp.buf.code_action, "Code actions")
+	map("<leader>q", vim.diagnostic.setqflist, "Populate quick fix list with diagnostics")
+	map("<leader>f", function()
+		vim.lsp.buf.format({
+			filter = function(client)
+				return client.name ~= "ts_ls"
+			end,
+		})
+	end, "Format buffer")
 end
 
-
 return {
-	'neovim/nvim-lspconfig',
+	"neovim/nvim-lspconfig",
 	opts = {
 		servers = {
-			ts_ls = { 
-				on_attach = function(client, bufnr) 
+			ts_ls = {
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			eslint = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			cssls = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			jsonls = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			gopls = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			svelte = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
+				end,
 			},
 			-- rust_analyzer = {
 			-- 	settings = {
@@ -70,19 +73,24 @@ return {
 			-- },
 			lua_ls = {
 				on_init = function(client)
-					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-						workspace = { library = { vim.env.VIMRUNTIME } }
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						workspace = { library = { vim.env.VIMRUNTIME } },
 					})
 				end,
 				on_attach = on_attach,
-				settings = { Lua = {} }
+				settings = { Lua = {} },
 			},
 			html = {
-				on_attach = function(client, bufnr) 
+				on_attach = function(client, bufnr)
 					common_on_attach(client, bufnr)
-				end
-			}
-		}
+				end,
+			},
+			slint_lsp = {
+				on_attach = function(client, bufnr)
+					common_on_attach(client, bufnr)
+				end,
+			},
+		},
 	},
 	inlay_hints = {
 		enabled = true,
@@ -95,4 +103,3 @@ return {
 		end
 	end,
 }
-
